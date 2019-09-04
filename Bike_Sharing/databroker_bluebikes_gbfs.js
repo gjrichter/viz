66e12be2007d9a65c7e92de1a427f35596614412
gjrichter <guenter.richter@gmail.com>
2019-09-04 12:17:34 +0200

    Create databroker_bluebikes_gbfs.js

---
+++ Bike_Sharing/databroker_bluebikes_gbfs.js
@@ -0,0 +1,52 @@
+﻿/**
+ * define namespace ixmaps
+ */
+
+window.ixmaps = window.ixmaps || {};
+(function() {
+
+		ixmaps.bike_stations = function(themeObj){
+
+			var myFeed = Data.feed({source:"https://gbfs.bluebikes.com/gbfs/en/station_information.json",
+					   type:"json",
+					  }).error(function(error){
+						ixmaps.error(error);
+					  }).load(function(json){
+
+						myFeed.__processJsonData(myFeed.data.data.stations,{"type":"json"});
+
+						ixmaps.setExternalData(myFeed.dbtable,{"type":"jsonDB","name":"bike_stations"});
+
+			});	// end Data.feed 1
+
+			return true;
+
+		};	// end ixmaps.bike_stations
+
+		ixmaps.bike_status = function(themeObj){
+
+			var myFeed = Data.feed({source:"https://gbfs.bluebikes.com/gbfs/en/station_status.json",
+					   type:"json",
+					  }).error(function(error){
+						ixmaps.error(error);
+					  }).load(function(json){
+
+						ixmaps.bikeUTC = myFeed.data.last_updated;
+						var d = new Date(ixmaps.bikeUTC*1000-8*60*60*1000);
+						themeObj.szTitle = "<p>"+d.toString().split("GMT")[0]+"</p>";
+						themeObj.szSnippet = "<p>BlueBikes Docking Station Status</p><hr>";
+
+						myFeed.__processJsonData(myFeed.data.data.stations,{"type":"json"});
+
+						ixmaps.setExternalData(myFeed.dbtable,{"type":"jsonDB","name":"bike_status"});
+
+						console.log("----- refresh -------");
+
+			});	// end Data.feed 1
+
+			return true;
+
+		};	// end ixmaps.bike_stations
+
+
+})();
