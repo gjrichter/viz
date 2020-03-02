14d632b763aa42895e5fa29e589016960a8b06ec
gjrichter <guenter.richter@gmail.com>
2020-03-02 19:14:16 +0100

    work in progress

---
+++ COVID-19/projects/process_rec_28.js
@@ -0,0 +1,38 @@
+/**
+ * data preprocessing 
+ * 
+ * get the last 28 columns from the data table
+ * and set the theme fields (columns) appropriate
+ */
+
+window.ixmaps = window.ixmaps || {};
+window.ixmaps.themeDataObj_rec = window.ixmaps.themeDataObj_rec || {};
+
+(function() {
+        
+    ixmaps.themeDataObj_rec.process = function(data,options) { 
+		
+		// get last 28 columns
+		var last_14 = data.columnNames().slice(-28);
+		
+		// set as data fields in actual theme
+		options.theme.szFields = last_14.slice().join("|");
+		options.theme.szFieldsA = last_14.slice();
+		
+		// make label ! -1 because of DIFFERENC theme
+		options.theme.szLabelA = last_14.slice(-27);
+		options.theme.szXaxisA = last_14.slice(-27);
+		for ( var i=1; i < options.theme.szXaxisA.length-1; i++ ){
+			options.theme.szXaxisA[i] = " ";
+		}
+     };   
+    
+})();
+
+/**
+ * end of namespace
+ */
+
+// -----------------------------
+// EOF
+// -----------------------------
