643ad656ba1111c1caca383ce1b842e9f8e8f123
gjrichter <guenter.richter@gmail.com>
2020-03-22 09:59:08 +0100

    wip

--- COVID-19/projects/databroker_CSSE_COVID.js
+++ COVID-19/projects/databroker_CSSE_COVID.js
@@ -86,17 +86,20 @@ window.ixmaps = window.ixmaps || {};
 		var szUrl1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 		var szUrl2 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
 		var szUrl3 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
+		var szUrl4 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 		
 		var broker = new Data.Broker()
 			.addSource(szUrl1, "csv")
 			.addSource(szUrl2, "csv")
 			.addSource(szUrl3, "csv")
+			.addSource(szUrl4, "csv")
 			.realize(
 				function (dataA) {
 
 					data_Confirmed = dataA[0];
 					data_Recovered = dataA[1];
 					data_Deaths = dataA[2];
+					data_Active = dataA[3];
 					
 					var lastDataColumnName = data_Confirmed.columnNames().pop();
 					
@@ -104,15 +107,23 @@ window.ixmaps = window.ixmaps || {};
 					//theme.szValueField = lastDataColumnName;	
 					theme.szDescription = "aggiornato: "+lastDataColumnName;
 					
+					var records = data_Active.records;
+					for ( var r=0; r<records.length; r++){
+						for ( var c=4; c<records[r].length; c++){
+							records[r][c] -= data_Recovered.records[r][c];
+							records[r][c] -= data_Deaths.records[r][c];
+						}
+					}
+
 					data_Confirmed.addColumn({destination:"type"},function(){return "Confirmed";});
 					data_Recovered.addColumn({destination:"type"},function(){return "Recovered";});
 					data_Deaths.addColumn({destination:"type"},function(){return "Deaths";});
+					data_Active.addColumn({destination:"type"},function(){return "Active";});
 					
 					data_Confirmed.append(data_Recovered);
 					data_Confirmed.append(data_Deaths);
+					data_Confirmed.append(data_Active);
 
-					console.log(data_Confirmed);	
-					
 					// -----------------------------------------------------------------------------------------------               
 					// deploy the data
 					// ----------------------------------------------------------------------------------------------- 
