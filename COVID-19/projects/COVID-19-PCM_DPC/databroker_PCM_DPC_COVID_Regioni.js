5fdd4ecaaf65c154b3fb92f6489fea2feadad06e
gjrichter <guenter.richter@gmail.com>
2020-04-03 17:42:53 +0200

    Update databroker_PCM_DPC_COVID_Regioni.js

--- COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
+++ COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
@@ -136,7 +136,7 @@ window.ixmaps = window.ixmaps || {};
 		var pivot = data.pivot(
 			{lead:"codice_regione",
 			 columns:"data",
-			 value:"totale_attualmente_positivi",
+			 value:"totale_positivi",
 			 keep:["lat","long","denominazione_regione"]}
 		);
 	
@@ -246,6 +246,23 @@ window.ixmaps = window.ixmaps || {};
 		}
 	
 		return homeTab;
+     };   
+
+	
+    var __mean_3 = function(table) { 
+		
+		// make mean of 3 days
+		var records = table.records;
+		for ( var r=0; r<records.length; r++ ){
+			for ( var c=4; c<records[r].length; c++){
+				records[r][c] = (records[r][c]+records[r][c+1]+records[r][c+2])/3;
+			}
+		}
+		var columns = table.columnNames();
+		table.column(columns.pop()).remove();
+		table.column(columns.pop()).remove();
+		
+		return table;
      };   
 	
 	
@@ -455,7 +472,9 @@ window.ixmaps = window.ixmaps || {};
 		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
 			
 			var pivot = __get_deaths_active_recovered(mydata,options);
-	
+			
+			pivot = __mean_3(pivot);
+
 			// get the columns with date 
 			var columns = pivot.columnNames();
 			
@@ -505,6 +524,60 @@ window.ixmaps = window.ixmaps || {};
 		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
 			
 			var pivot = __get_deaths_active(mydata,options);
+
+			pivot = __mean_3(pivot);
+	
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			columns.shift();
+			columns.shift();
+			columns.pop();
+			
+			var last = columns.length-1;
+			
+			// and configure the theme
+			theme.szFields = columns.slice().join('|');
+			theme.szFieldsA = columns.slice();
+			
+			// and set the label (for difference 1 less)
+			columns.shift();
+			theme.szLabelA = columns.slice();
+			
+			theme.szSnippet = "dal "+columns[0]+" al "+columns[last-1];
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+
+		})
+		.error(function(e){alert("error loading data from:\n"+szUrl)});
+
+	};
+
+	ixmaps.PCM_DPC_COVID_SEQUENCE_DEATHS = function (theme,options) {
+
+
+		var szUrl = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
+			
+			var pivot = __get_deaths(mydata,options);
+			
+			pivot = __mean_3(pivot);
 	
 			// get the columns with date 
 			var columns = pivot.columnNames();
@@ -556,6 +629,8 @@ window.ixmaps = window.ixmaps || {};
 			
 			var pivot = __get_deaths_home_hospital_intensive_recovered(mydata,options);
 	
+			pivot = __mean_3(pivot);
+
 			// get the columns with date 
 			var columns = pivot.columnNames();
 			
@@ -606,6 +681,8 @@ window.ixmaps = window.ixmaps || {};
 			
 			var pivot = __get_deaths_home_hospital_intensive(mydata,options);
 	
+			pivot = __mean_3(pivot);
+
 			// get the columns with date 
 			var columns = pivot.columnNames();
 			
@@ -656,6 +733,8 @@ window.ixmaps = window.ixmaps || {};
 			
 			var pivot = __get_deaths_home_hospital(mydata,options);
 	
+			pivot = __mean_3(pivot);
+
 			// get the columns with date 
 			var columns = pivot.columnNames();
 			
@@ -706,6 +785,8 @@ window.ixmaps = window.ixmaps || {};
 			
 			var pivot = __get_deaths_home(mydata,options);
 	
+			pivot = __mean_3(pivot);
+
 			// get the columns with date 
 			var columns = pivot.columnNames();
 			
@@ -756,6 +837,8 @@ window.ixmaps = window.ixmaps || {};
 			
 			var pivot = __get_hospitalized(mydata,options);
 	
+			pivot = __mean_3(pivot);
+
 			// get the columns with date 
 			var columns = pivot.columnNames();
 			
@@ -806,6 +889,8 @@ window.ixmaps = window.ixmaps || {};
 			
 			var pivot = __get_intensive(mydata,options);
 	
+			pivot = __mean_3(pivot);
+
 			// get the columns with date 
 			var columns = pivot.columnNames();
 			
@@ -843,56 +928,6 @@ window.ixmaps = window.ixmaps || {};
 
 	};
 	
-	ixmaps.PCM_DPC_COVID_SEQUENCE_DEATHS = function (theme,options) {
-
-
-		var szUrl = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv";
-
-		// -----------------------------------------------------------------------------------------------               
-		// read the ArcGis Feature service
-		// ----------------------------------------------------------------------------------------------- 
-
-		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
-			
-			var pivot = __get_deaths(mydata,options);
-	
-			// get the columns with date 
-			var columns = pivot.columnNames();
-			
-			// get the columns with date 
-			var columns = pivot.columnNames();
-			columns.shift();
-			columns.shift();
-			columns.shift();
-			columns.shift();
-			columns.pop();
-			
-			var last = columns.length-1;
-			
-			// and configure the theme
-			theme.szFields = columns.slice().join('|');
-			theme.szFieldsA = columns.slice();
-			
-			// and set the label (for difference 1 less)
-			columns.shift();
-			theme.szLabelA = columns.slice();
-			
-			theme.szSnippet = "dal "+columns[0]+" al "+columns[last-1];
-
-			// -----------------------------------------------------------------------------------------------               
-			// deploy the data
-			// ----------------------------------------------------------------------------------------------- 
-
-			ixmaps.setExternalData(pivot, {
-				type: "dbtable",
-				name: options.name
-			});
-
-		})
-		.error(function(e){alert("error loading data from:\n"+szUrl)});
-
-	};
-
 	ixmaps.PCM_DPC_COVID_ACTUAL = function (theme,options) {
 
 
