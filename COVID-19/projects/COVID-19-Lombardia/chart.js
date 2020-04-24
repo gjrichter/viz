a7919e052919cf52c863b3c741043b57f6de479a
gjrichter <guenter.richter@gmail.com>
2020-04-24 23:04:24 +0200

    Update chart.js

--- COVID-19/projects/COVID-19-Lombardia/chart.js
+++ COVID-19/projects/COVID-19-Lombardia/chart.js
@@ -38,6 +38,9 @@ window.ixmaps = window.ixmaps || {};
 	// here we define the gradient for filling the peeks
 	// --------------------------------------------------
 	
+	ixmaps.testuserdraw_init = function(SVGDocument,args){
+		return ixmaps.htmlgui_initChart(SVGDocument,args);
+	}
 	ixmaps.htmlgui_initChart = function(SVGDocument,args){
 		
 		var szColor = args.theme.colorScheme[0];
