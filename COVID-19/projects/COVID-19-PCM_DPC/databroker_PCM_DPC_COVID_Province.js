2b95ceaf7e1122bc4ee9b84c01356c629996acb2
gjrichter <guenter.richter@gmail.com>
2020-06-30 23:08:25 +0200

    test

--- COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Province.js
+++ COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Province.js
@@ -14,12 +14,6 @@ window.ixmaps = window.ixmaps || {};
 			return value.split(" ")[0];
 		});
         
-        data.column("lat").map(function (value) {
-            return value.substr(0,9);
-        });
-        data.column("long").map(function (value) {
-            return value.substr(0,9);
-        });
         
  		var pivot = data.pivot({
 			lead: "codice_provincia",
