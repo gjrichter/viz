115fe5f4cd0c09078d85b5df14476db6676f8b5b
gjrichter <guenter.richter@gmail.com>
2020-07-31 19:12:48 +0200

    Update databroker_PCM_DPC_COVID_Regioni.js

--- COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
+++ COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
@@ -2767,7 +2767,7 @@ window.ixmaps = window.ixmaps || {};
 	ixmaps.PCM_DPC_COVID_SEQUENCE_MEAN_7 = function (theme, options) {
 
 
-		var szUrl = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-regioni.csv";
+		var szUrl = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv";
 
 		// -----------------------------------------------------------------------------------------------               
 		// read the ArcGis Feature service
