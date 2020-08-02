7086937bd72d56273fb7b725e9c4d7d80848bdef
gjrichter <guenter.richter@gmail.com>
2020-08-02 18:04:50 +0200

    new project

--- COVID-19/projects/COVID-19-NYT/databroker_NYT_COVID.js
+++ COVID-19/projects/COVID-19-NYT/databroker_NYT_COVID.js
@@ -458,6 +458,96 @@ window.ixmaps = window.ixmaps || {};
 				});
 	};
 
+	ixmaps.NYT_COVID_SEQUENCE_RM7_D = function (theme, options) {
+
+		var szUrl1 = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv";
+
+		var broker = new Data.Broker()
+			.addSource(szUrl1, "csv")
+			.realize(
+				function (dataA) {
+
+					data = dataA[0];
+					
+					var pivot = __get_pivot_corr("deaths");
+					
+					// get the columns with date 
+					var columns = pivot.columnNames();
+					var last = columns.length - 1;
+					columns.shift();
+					columns.shift();
+
+					records = pivot.records;
+					for ( r=0; r<records.length; r++ ){
+						for ( c=last; c>=8; c-- ){
+							var mean = (Number(records[r][c]  )+Number(records[r][c-1])+Number(records[r][c-2])+Number(records[r][c-3])+Number(records[r][c-4])+Number(records[r][c-5])+Number(records[r][c-6]))/7;
+							records[r][c] = mean.toFixed(2);
+						}
+					}
+					columns.shift();
+					columns.shift();
+					columns.shift();
+					columns.shift();
+					columns.shift();
+					columns.shift();
+
+					// and configure the theme
+					theme.szFields = columns.slice().join('|');
+					theme.szFieldsA = columns.slice();
+
+					// prepare label and xaxis for DIFFERENCE theme 
+					columns.shift();
+					var last = columns.length - 1;
+					
+					// and set the label
+					theme.szLabelA = columns.slice();
+					
+					theme.szSnippet = "from " + columns[0] + " to " + columns[last];
+					ixmaps.setTitle("<span style='font-family:arial'>" + columns[0] + " to " + columns[last] +"</span>");
+					
+					var szXaxisA = [];
+					for ( var i =0; i<columns.length; i++ ){
+                        if (columns[i] == "2020-03-01"){
+  						  szXaxisA.push("March");
+                        }else
+                        if (columns[i] == "2020-04-01"){
+  						  szXaxisA.push("April");
+                        }else
+                        if (columns[i] == "2020-05-01"){
+  						  szXaxisA.push("May");
+                        }else
+                        if (columns[i] == "2020-06-01"){
+  						  szXaxisA.push("June");
+                        }else
+                        if (columns[i] == "2020-07-01"){
+  						  szXaxisA.push("July");
+                        }else
+                        if (columns[i] == "2020-08-01"){
+  						  szXaxisA.push("Aug");
+                        }else
+                        if (columns[i] == "2020-09-01"){
+  						  szXaxisA.push("Sept");
+                        }else
+                        if (columns[i] == "2020-010-01"){
+  						  szXaxisA.push("Okt");
+                        }else{
+						  szXaxisA.push(" ");
+                        }
+					}
+					//szXaxisA[0] = (columns[0]);
+					//szXaxisA[columns.length-1] = (columns[columns.length-1]);
+					theme.szXaxisA = szXaxisA;
+								 
+					// -----------------------------------------------------------------------------------------------               
+					// deploy the data
+					// ----------------------------------------------------------------------------------------------- 
+
+					ixmaps.setExternalData(pivot, {
+						type: "dbtable",
+						name: options.name
+					});
+				});
+	};
 
 
 
