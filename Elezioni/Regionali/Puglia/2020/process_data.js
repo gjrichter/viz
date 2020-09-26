e7bc85523720bb44e9a3a64be60115120086c25c
gjrichter <guenter.richter@gmail.com>
2020-09-26 19:29:52 +0200

    new viz: Elezioni Regionali Puglia 2020

---
+++ Elezioni/Regionali/Puglia/2020/process_data.js
@@ -0,0 +1,34 @@
+/** 
+ * @fileoverview This file provides processing functions for ixmaps 
+ * @author Guenter Richter guenter.richter@medienobjekte.de
+ * @version 1.0 
+ */
+
+/**
+ * define namespace ixmaps
+ */
+
+window.ixmaps = window.ixmaps || {};
+window.ixmaps.themeDataObj = window.ixmaps.themeDataObj || {};
+
+(function() {
+    
+ 	ixmaps.themeDataObj.process = function(dbtable) {
+		dbtable = dbtable.select("WHERE IDREGIONE is 16");
+		dbtable = dbtable.pivot({  
+								"lead":	'CODICE ISTAT',
+								"cols":	'cogn',
+								"value": 'voti' 
+                                });
+		return dbtable;
+ 	};
+
+})();
+
+/**
+ * end of namespace
+ */
+
+// -----------------------------
+// EOF
+// -----------------------------
