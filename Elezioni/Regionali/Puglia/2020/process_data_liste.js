d8666b75f9a42e5940048a05f2655a757bf285e6
gjrichter <guenter.richter@gmail.com>
2020-09-26 20:09:52 +0200

    Create process_data_liste.js

---
+++ Elezioni/Regionali/Puglia/2020/process_data_liste.js
@@ -0,0 +1,49 @@
+/** 
+ * @fileoverview This file provides processing functions for ixmaps 
+ * @author Guenter Richter guenter.richter@medienobjekte.de
+ * @version 1.0 
+ */
+
+/**
+ * define namespace ixmaps
+ */
+
+window.ixmaps = window.ixmaps || {};
+window.ixmaps.themeDataObj = window.ixmaps.themeDataObj || {};
+window.ixmaps.themeDataObj_liste = window.ixmaps.themeDataObj_liste || {};
+
+(function() {
+    
+ 	ixmaps.themeDataObj.process = function(dbtable) { 
+		dbtable = dbtable.select("WHERE IDREGIONE is 16");
+		dbtable.column("Denominazione dell'Unità territoriale sovracomunale (valida a fini statistici)").rename("comune");
+		dbtable = dbtable.pivot({  
+								"lead":	'CODICE ISTAT',
+								"cols":	'desc_lis_c',
+								"keep": 'comune',
+								"value": 'voti' 
+                                });
+		return dbtable;
+ 	};
+	
+ 	ixmaps.themeDataObj_liste.process = function(dbtable) { 
+		dbtable = dbtable.select("WHERE IDREGIONE is 16");
+		dbtable.column("Denominazione dell'Unità territoriale sovracomunale (valida a fini statistici)").rename("comune");
+		dbtable = dbtable.pivot({  
+								"lead":	'CODICE ISTAT',
+								"cols":	'desc_lis_c',
+								"keep": 'comune',
+								"value": 'voti' 
+                                });
+		return dbtable;
+ 	};
+
+})();
+
+/**
+ * end of namespace
+ */
+
+// -----------------------------
+// EOF
+// -----------------------------
