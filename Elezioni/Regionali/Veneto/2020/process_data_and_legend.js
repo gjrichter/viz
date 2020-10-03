1dd0be9943e4fde774e899f9a313f032d102e05f
gjrichter <guenter.richter@gmail.com>
2020-10-03 14:53:51 +0200

    new project

---
+++ Elezioni/Regionali/Veneto/2020/process_data_and_legend.js
@@ -0,0 +1,42 @@
+/** 
+ * @fileoverview This file provides processing functions for ixmaps 
+ * @author Guenter Richter guenter.richter@medienobjekte.de
+ * @version 1.0 
+ */
+
+/**
+ * define namespace ixmaps
+ */
+
+window.ixmaps = window.ixmaps || {};
+window.ixmaps.themeDataObj = window.ixmaps.themeDataObj || {};
+
+(function() {
+    
+ 	ixmaps.themeDataObj.process = function(dbtable) {
+		dbtable = dbtable.select("WHERE IDREGIONE is 5");
+		dbtable = dbtable.pivot({  
+								"lead":	'CODICE ISTAT',
+								"cols":	'cogn',
+								"value": 'voti' 
+                                });
+		return dbtable;
+ 	};
+	ixmaps.htmlgui_onLegendFooter = function() {
+	 	return ("<div class='map-legend-footer' style='margin-top:0.5em;'>"+
+				"<a href=\"javascript:ixmaps.map().changeThemeStyle('polygon','dopacitypow:-0.1','add')\" class=\"button\" style=\"pointer-events:all\">"+
+				"(-)</a>&nbsp;fondo&nbsp;"+
+				"<a href=\"javascript:ixmaps.map().changeThemeStyle('polygon','dopacitypow:+0.1','add')\" class=\"button\" style=\"pointer-events:all\">"+
+				"(+)</a>"+
+			 	"</div>");
+    };
+
+})();
+
+/**
+ * end of namespace
+ */
+
+// -----------------------------
+// EOF
+// -----------------------------
