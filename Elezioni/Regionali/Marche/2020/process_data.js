8d0b4beb4018dcff377ae3eba3dbc3c7a0c3f992
gjrichter <guenter.richter@gmail.com>
2020-10-12 17:04:49 +0200

    Create process_data.js

---
+++ Elezioni/Regionali/Marche/2020/process_data.js
@@ -0,0 +1,33 @@
+/** 
+ * @fileoverview This file provides processing functions for ixmaps 
+ * @author Guenter Richter guenter.richter@medienobjekte.de
+ * @version 1.0 
+ */
+
+/**
+ * define namespace ixmaps
+ */
+
+window.ixmaps = window.ixmaps || {};
+window.ixmaps.themeDataObj = window.ixmaps.themeDataObj || {};
+
+(function() {
+    
+ 	ixmaps.themeDataObj.process = function(dbtable) {
+		dbtable = dbtable.pivot({  
+								"lead":	'PRO_COM_T',
+								"cols":	'cognome_cand',
+								"value": 'voti' 
+                                });
+		return dbtable;
+ 	};
+
+})();
+
+/**
+ * end of namespace
+ */
+
+// -----------------------------
+// EOF
+// -----------------------------
