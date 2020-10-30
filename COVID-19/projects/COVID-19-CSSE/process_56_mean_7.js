c867b63ebceb2032a2f39469edb5e61ffe3e799c
gjrichter <guenter.richter@gmail.com>
2020-10-30 10:41:15 +0100

    Update process_56_mean_7.js

--- COVID-19/projects/COVID-19-CSSE/process_56_mean_7.js
+++ COVID-19/projects/COVID-19-CSSE/process_56_mean_7.js
@@ -51,7 +51,7 @@ window.ixmaps.CSSE_COVID_CONFIRMED_56 = window.ixmaps.CSSE_COVID_CONFIRMED_56 ||
 		options.theme.szLabelA = last_56.slice(-55);
 		options.theme.szXaxisA = last_56.slice(-55);
 		for ( var i=1; i < options.theme.szXaxisA.length-1; i++ ){
-			options.theme.szXaxisA[i] = i%14?" ":new Date(options.theme.szXaxisA[i]).toLocaleDateString().split("/").pop().join('.');
+			options.theme.szXaxisA[i] = i%14?" ":new Date(options.theme.szXaxisA[i]).toLocaleDateString();
 		}
      };   
     
