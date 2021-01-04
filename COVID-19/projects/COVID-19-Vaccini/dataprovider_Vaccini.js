062335fcd1a4721f6879c6b97dbe04e14647c41e
gjrichter <guenter.richter@gmail.com>
2021-01-04 20:03:12 +0100

    work in progress

--- COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini.js
+++ COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini.js
@@ -20,7 +20,7 @@ window.ixmaps = window.ixmaps || {};
 		return pivot;
      };   
 
-	
+
     ixmaps.VACCINI_SOMMINISTRAZIONI_LAST = function (theme,options) {
 
 
@@ -115,6 +115,147 @@ window.ixmaps = window.ixmaps || {};
 		.error(function(e){alert("error loading data from:\n"+szUrl);});
 
 	};
+	
+    ixmaps.VACCINI_PERCENTUALI_CLIP = function (theme,options) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/COVID-19/consegnate.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+
+			var data = dataA[0];	
+			var pivot = __get_somministrazioni(data,options);
+			var conseg= dataA[1];
+				
+			var consegA = conseg.lookupArray("assegnati","codice_regione");	
+			console.log(conseg);
+			console.log(consegA);
+			var indexCodice = pivot.column("codice_regione").index;	
+			var records = pivot.records;
+			for ( var r=0; r<records.length; r++ ){
+				for ( var c=2; c<records[r].length-1; c++ ){
+					records[r][c] = (Number(records[r][c]) / consegA[Number(records[r][indexCodice])]*100).toFixed(2);
+ 				}
+			}
+				
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			columns.pop();
+			
+			var last = columns.length-1;
+
+			// and configure the theme
+			theme.szFields = columns.slice().join('|');
+			theme.szFieldsA = columns.slice();
+			
+			// and set the label (for difference 1 less)
+			//columns.shift();
+			theme.szLabelA = columns.slice();
+			
+			for ( var i=0; i<columns.length; i++ ){
+				columns[i] = new Date(columns[i]).toLocaleDateString();
+			}
+			theme.szLabelA = columns.slice();
+			
+			theme.szXaxisA = columns.slice();
+			
+			theme.nClipFrames = columns.length;
+			
+			theme.szSnippet = "dal "+columns[0]+" al "+columns[last];
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+		});
+	};
+	
+    ixmaps.VACCINI_PERCENTUALI_SEQUENCE = function (theme,options) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/COVID-19/consegnate.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+
+			var data = dataA[0];	
+			var pivot = __get_somministrazioni(data,options);
+			var conseg= dataA[1];
+				
+			var consegA = conseg.lookupArray("assegnati","codice_regione");	
+			console.log(conseg);
+			console.log(consegA);
+			var indexCodice = pivot.column("codice_regione").index;	
+			var records = pivot.records;
+			for ( var r=0; r<records.length; r++ ){
+				for ( var c=2; c<records[r].length-1; c++ ){
+					records[r][c] = (Number(records[r][c]) / consegA[Number(records[r][indexCodice])]*100).toFixed(2);
+ 				}
+			}
+				
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			columns.pop();
+			
+			var last = columns.length-1;
+
+			// and configure the theme
+			theme.szFields = columns.slice().join('|');
+			theme.szFieldsA = columns.slice();
+			
+			// and set the label (for difference 1 less)
+			//columns.shift();
+			theme.szLabelA = columns.slice();
+			
+			for ( var i=0; i<columns.length; i++ ){
+				columns[i] = new Date(columns[i]).toLocaleDateString();
+			}
+			theme.szLabelA = columns.slice();
+			
+			//theme.szXaxisA = columns.slice();
+			
+			theme.nClipFrames = columns.length;
+			
+			theme.szSnippet = "dal "+columns[0]+" al "+columns[last];
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+		});
+	};
+	
 
 })();
 
