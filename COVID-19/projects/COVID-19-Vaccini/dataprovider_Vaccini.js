a2c3af114c41e863ae747d37c73733b676f0e00c
gjrichter <guenter.richter@gmail.com>
2021-01-07 17:26:00 +0100

    Update dataprovider_Vaccini.js

--- COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini.js
+++ COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini.js
@@ -9,7 +9,6 @@ window.ixmaps = window.ixmaps || {};
 
     var __get_somministrazioni = function(data,options) { 
 
-	
 		var pivot = data.pivot(
 			{lead:"codice_regione",
 			 columns:"dataAggiornamento",
@@ -20,6 +19,29 @@ window.ixmaps = window.ixmaps || {};
 		return pivot;
      };   
 
+    var __get_percentuali = function(data,options) { 
+	
+		var pivot = data.pivot(
+			{lead:"codice_regione",
+			 columns:"dataAggiornamento",
+			 value:"percentuale",
+			 keep:["regione"]}
+		);
+	
+		return pivot;
+     };   
+
+     var __get_consegnate = function(data,options) { 
+	
+		var pivot = data.pivot(
+			{lead:"codice_regione",
+			 columns:"dataAggiornamento",
+			 value:"dosiConsegnate",
+			 keep:["regione"]}
+		);
+	
+		return pivot;
+     };   
 
     ixmaps.VACCINI_SOMMINISTRAZIONI_LAST = function (theme,options) {
 
@@ -33,6 +55,42 @@ window.ixmaps = window.ixmaps || {};
 		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
 			
 			var pivot = __get_somministrazioni(mydata,options);
+	
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.pop();
+			
+			var last = columns.length-1;
+		
+			theme.szFields = columns[last];
+			theme.szFieldsA = [columns[last]];
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+
+		})
+		.error(function(e){alert("error loading data from:\n"+szUrl);});
+
+	};
+		
+    ixmaps.VACCINI_CONSEGNATE_LAST = function (theme,options) {
+
+
+		var szUrl = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
+			
+			var pivot = __get_consegnate(mydata,options);
 	
 			// get the columns with date 
 			var columns = pivot.columnNames();
@@ -74,6 +132,60 @@ window.ixmaps = window.ixmaps || {};
 		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
 			
 			var pivot = __get_somministrazioni(mydata,options);
+	
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			columns.pop();
+			
+			var last = columns.length-1;
+
+			// and configure the theme
+			theme.szFields = columns.slice().join('|');
+			theme.szFieldsA = columns.slice();
+			
+			// and set the label (for difference 1 less)
+			//columns.shift();
+			theme.szLabelA = columns.slice();
+			
+			for ( var i=0; i<columns.length; i++ ){
+				columns[i] = new Date(columns[i]).toLocaleDateString();
+			}
+			theme.szLabelA = columns.slice();
+			
+			theme.szXaxisA = columns.slice();
+			
+			theme.nClipFrames = columns.length;
+			
+			theme.szSnippet = "dal "+columns[0]+" al "+columns[last];
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+
+		})
+		.error(function(e){alert("error loading data from:\n"+szUrl);});
+
+	};
+	
+    ixmaps.VACCINI_CONSEGNATE_CLIP = function (theme,options) {
+
+
+		var szUrl = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var myfeed = Data.feed({"source":szUrl,"type":"csv"}).load(function(mydata){
+			
+			var pivot = __get_consegnate(mydata,options);
 	
 			// get the columns with date 
 			var columns = pivot.columnNames();
@@ -240,6 +352,244 @@ window.ixmaps = window.ixmaps || {};
 		});
 	};
 	
+    ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_CLIP = function (theme,options) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/ISTAT/DCIS_POPRES1_13032020145850184.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+
+			var data = dataA[0];	
+			var pivot = __get_somministrazioni(data,options);
+
+			// get population lookup for incidence
+			var dataPop = dataA[1];
+			// correct region names in population table
+			dataPop.column("Territorio").map(function (value) {
+				if (value == "Provincia Autonoma Bolzano / Bozen") {
+					return "P.A. Bolzano";
+				} else
+				if (value == "Provincia Autonoma Trento") {
+					return "P.A. Trento";
+				} else {
+					return value.split(" /")[0].replace(/-/, " ");
+				}
+			});
+			var pop = [];
+			var terrA = dataPop.column("Territorio").values();
+			var popA = dataPop.column("Value").values();
+			for (var i = 0; i < terrA.length; i++) {
+				pop[terrA[i]] = popA[i];
+			}
+
+			pivot.column("Total").remove();
+			var indexName = pivot.column("regione").index;
+
+			var records = pivot.records;
+			for ( var r=0; r<records.length; r++ ){
+				for ( var c=2; c<records[r].length; c++ ){
+					records[r][c] = (Number(records[r][c]) / pop[records[r][indexName].replace(/\-/," ")]*100).toFixed(2);
+ 				}
+			}
+
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			
+			var last = columns.length-1;
+
+			// and configure the theme
+			theme.szFields = columns.slice().join('|');
+			theme.szFieldsA = columns.slice();
+			
+			var max = pivot.column(columns[last]).values().sort()[0];
+			ixmaps.changeThemeStyle(theme.szId,"normalsizevalue:"+Math.ceil(max),"set")
+
+			// and set the label (for difference 1 less)
+			theme.szLabelA = columns.slice();
+			
+			for ( var i=0; i<columns.length; i++ ){
+				columns[i] = new Date(columns[i]).toLocaleDateString();
+			}
+			theme.szLabelA = columns.slice();
+			
+			theme.szXaxisA = columns.slice();
+			
+			theme.nClipFrames = columns.length;
+			
+			theme.szSnippet = "dal "+columns[0]+" al "+columns[last];
+				
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+		});
+	};
+	
+    ixmaps.VACCINI_PERCENTUALI_CONSEGNATE_POPOLAZIONE_LAST = function (theme,options) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/ISTAT/DCIS_POPRES1_13032020145850184.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+
+			var data = dataA[0];	
+			var pivot = __get_consegnate(data,options);
+
+			// get population lookup for incidence
+			var dataPop = dataA[1];
+			// correct region names in population table
+			dataPop.column("Territorio").map(function (value) {
+				if (value == "Provincia Autonoma Bolzano / Bozen") {
+					return "P.A. Bolzano";
+				} else
+				if (value == "Provincia Autonoma Trento") {
+					return "P.A. Trento";
+				} else {
+					return value.split(" /")[0].replace(/-/, " ");
+				}
+			});
+			var pop = [];
+			var terrA = dataPop.column("Territorio").values();
+			var popA = dataPop.column("Value").values();
+			for (var i = 0; i < terrA.length; i++) {
+				pop[terrA[i]] = popA[i];
+			}
+
+			pivot.column("Total").remove();
+			var indexName = pivot.column("regione").index;
+
+			var records = pivot.records;
+			for ( var r=0; r<records.length; r++ ){
+				for ( var c=2; c<records[r].length; c++ ){
+					records[r][c] = (Number(records[r][c]) / pop[records[r][indexName].replace(/\-/," ")]*100).toFixed(2);
+ 				}
+			}
+
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			
+			var last = columns.length-1;
+		
+			theme.szFields = columns[last];
+			theme.szFieldsA = [columns[last]];
+				
+			var max = pivot.column(columns[last]).values().sort()[0];
+			theme.nNormalSizeValue = Math.ceil(max);	
+				
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+
+		});
+	};
+    ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_LAST = function (theme,options) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/ISTAT/DCIS_POPRES1_13032020145850184.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+
+			var data = dataA[0];	
+			var pivot = __get_somministrazioni(data,options);
+
+			// get population lookup for incidence
+			var dataPop = dataA[1];
+			// correct region names in population table
+			dataPop.column("Territorio").map(function (value) {
+				if (value == "Provincia Autonoma Bolzano / Bozen") {
+					return "P.A. Bolzano";
+				} else
+				if (value == "Provincia Autonoma Trento") {
+					return "P.A. Trento";
+				} else {
+					return value.split(" /")[0].replace(/-/, " ");
+				}
+			});
+			var pop = [];
+			var terrA = dataPop.column("Territorio").values();
+			var popA = dataPop.column("Value").values();
+			for (var i = 0; i < terrA.length; i++) {
+				pop[terrA[i]] = popA[i];
+			}
+
+			pivot.column("Total").remove();
+			var indexName = pivot.column("regione").index;
+
+			var records = pivot.records;
+			for ( var r=0; r<records.length; r++ ){
+				for ( var c=2; c<records[r].length; c++ ){
+					records[r][c] = (Number(records[r][c]) / pop[records[r][indexName].replace(/\-/," ")]*100).toFixed(2);
+ 				}
+			}
+
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			
+			var last = columns.length-1;
+		
+			theme.szFields = columns[last];
+			theme.szFieldsA = [columns[last]];
+				
+			var max = pivot.column(columns[last]).values().sort()[0];
+			theme.nNormalSizeValue = Math.ceil(max);	
+				
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+
+		});
+	};
     ixmaps.VACCINI_PERCENTUALI_SEQUENCE = function (theme,options) {
 		
 		var szUrl1 = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
@@ -314,6 +664,100 @@ window.ixmaps = window.ixmaps || {};
 			});
 		});
 	};
+
+   ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_SEQUENCE = function (theme,options) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/processing/somministrazioni.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/ISTAT/DCIS_POPRES1_13032020145850184.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+
+			var data = dataA[0];	
+			var pivot = __get_somministrazioni(data,options);
+
+			// get population lookup for incidence
+			var dataPop = dataA[1];
+			// correct region names in population table
+			dataPop.column("Territorio").map(function (value) {
+				if (value == "Provincia Autonoma Bolzano / Bozen") {
+					return "P.A. Bolzano";
+				} else
+				if (value == "Provincia Autonoma Trento") {
+					return "P.A. Trento";
+				} else {
+					return value.split(" /")[0].replace(/-/, " ");
+				}
+			});
+			var pop = [];
+			var terrA = dataPop.column("Territorio").values();
+			var popA = dataPop.column("Value").values();
+			for (var i = 0; i < terrA.length; i++) {
+				pop[terrA[i]] = popA[i];
+			}
+
+			pivot.column("Total").remove();
+			var indexName = pivot.column("regione").index;
+
+			var records = pivot.records;
+			for ( var r=0; r<records.length; r++ ){
+				for ( var c=2; c<records[r].length; c++ ){
+					records[r][c] = (Number(records[r][c]) / pop[records[r][indexName].replace(/\-/," ")]*100).toFixed(2);
+ 				}
+			}
+
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+			
+			var last = columns.length-1;
+
+			// and configure the theme
+			theme.szFields = columns.slice().join('|');
+			theme.szFieldsA = columns.slice();
+			
+			// and set the label (for difference 1 less)
+			//columns.shift();
+			theme.szLabelA = columns.slice();
+			
+			for ( var i=0; i<columns.length; i++ ){
+				columns[i] = new Date(columns[i]).toLocaleDateString();
+			}
+			theme.szLabelA = columns.slice();
+			
+			// make curve xaxis 
+			var xAxis = [];
+			for ( i in columns ){
+				xAxis.push(" ");
+			}
+			xAxis[columns.length-1]=columns[columns.length-1];
+			xAxis[0]=columns[0];
+			options.theme.szXaxisA = xAxis; 
+			
+			theme.szSnippet = "dal "+columns[0]+" al "+columns[last];
+
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+		});
+	};
+	
 	
 
 })();
