4b1744f2336f3085f04ffb200a1a97bfb56b4285
gjrichter <guenter.richter@gmail.com>
2021-01-29 19:08:50 +0100

    Update dataprovider_Vaccini_opendata.js

--- COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini_opendata.js
+++ COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini_opendata.js
@@ -96,6 +96,82 @@ window.ixmaps = window.ixmaps || {};
 		.error(function(e){alert("error loading data from:\n"+szUrl);});
 
 	};
+	
+	    ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_LAST = function (theme,options) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/ISTAT/DCIS_POPRES1_13032020145850184.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+
+			var mydata = dataA[0];
+				
+			var dateA = mydata.column("data_somministrazione").values();
+			var date = dateA.pop();
+			date = new Date(date).toLocaleDateString();
+			ixmaps.setTitle("<f2 style='color:#888888;background-color:rgba(255,255,255,0.1);padding:0.3em 0.5em;border:#888888 solid 0.5px;border-radius:0.2em'>aggiornato: "+date+"</f2>");
+			
+			mydata.condense('area',{keep:"area"});
+			
+			mydata.addColumn({source:"area",destination:"NUTS"},function(value){
+				return areaToNuts[value];	
+			});
+
+			mydata.addColumn({source:"area",destination:"Nome"},function(value){
+				return areaToNome[value];	
+			});
+				
+			// get population lookup for incidence
+			var dataPop = dataA[1];
+			// correct region names in population table
+			dataPop.column("Territorio").map(function (value) {
+				if (value == "Provincia Autonoma Bolzano / Bozen") {
+					return "Provincia Autonoma di Bolzano/Bozen";
+				} else
+				if (value == "Provincia Autonoma Trento") {
+					return "Provincia Autonoma di Trento";
+				} else
+				if (value == "Valle d'Aosta / Vallée d'Aoste") {
+					return "Valle d'Aosta/Vallée d'Aoste";
+				} else {
+					return value.split(" /")[0].replace(/-/, " ");
+				}
+			});
+			var pop = [];
+			var terrA = dataPop.column("Territorio").values();
+			var popA = dataPop.column("Value").values();
+			for (var i = 0; i < terrA.length; i++) {
+				pop[terrA[i]] = popA[i];
+			}
+
+			var iName  = mydata.column("Nome").index;
+			var iValue = mydata.column("prima_dose").index;
+			mydata.addColumn({destination:"prima_dose_pop"},function(row){
+				return (Number(row[iValue]) / pop[row[iName].replace(/\-/," ")]*100).toFixed(2);	
+			});
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(mydata, {
+				type: "dbtable",
+				name: options.name
+			});
+
+		});
+	};
+
 		
 })();
 
