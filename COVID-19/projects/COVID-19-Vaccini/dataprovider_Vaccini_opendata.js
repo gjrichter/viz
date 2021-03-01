03ddeaba0a3582156afbf458d2c84b483bd3ee56
gjrichter <guenter.richter@gmail.com>
2021-03-01 22:30:41 +0100

    Update dataprovider_Vaccini_opendata.js

--- COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini_opendata.js
+++ COVID-19/projects/COVID-19-Vaccini/dataprovider_Vaccini_opendata.js
@@ -136,7 +136,7 @@ window.ixmaps = window.ixmaps || {};
 	};
 	
 	
-	ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_LAST = function (theme,options) {
+	ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_LAST = function (theme,options,column) {
 		
 		var szUrl1 = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.csv";
 		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/ISTAT/DCIS_POPRES1_13032020145850184.csv";
@@ -185,12 +185,6 @@ window.ixmaps = window.ixmaps || {};
 			mydata.addColumn({destination:"prima_dose_pop"},function(row){
 				return (Number(row[iValue]) / pop[row[iName].replace(/\-/," ")]*100).toFixed(2);	
 			});
-				
-				iName  = mydata.column("nome_area").index;
-				iValue = mydata.column("seconda_dose").index;
-			mydata.addColumn({destination:"seconda_dose_pop"},function(row){
-				return (Number(row[iValue]) / pop[row[iName].replace(/\-/," ")]*100).toFixed(2);	
-			});
 
 			// -----------------------------------------------------------------------------------------------               
 			// deploy the data
@@ -204,6 +198,14 @@ window.ixmaps = window.ixmaps || {};
 		});
 	};
 	
+	ixmaps.VACCINI_FIRST_PERCENTUALI_POPOLAZIONE_LAST = function(theme,options) {
+		return ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_LAST(theme,options,"prima_dose_pop");
+	};
+	
+	ixmaps.VACCINI_SECOND_PERCENTUALI_POPOLAZIONE_LAST = function(theme,options) {
+		return ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_LAST(theme,options,"seconda_dose_pop");
+	};	
+
     var __mean_3 = function(table) { 
 		
 		// make mean of 3 days
@@ -327,6 +329,14 @@ window.ixmaps = window.ixmaps || {};
 																"categoria_ospiti_rsa",
 																"categoria_over80"],"mean3");
 	};
+	ixmaps.VACCINI_SOMMINISTRAZIONI_ALL_NEW_SEQUENCE_MEAN_3 = function (theme, options) {
+		return ixmaps.VACCINI_COLUMNS_SEQUENCE(theme, options, ["categoria_operatori_sanitari_sociosanitari",
+																"categoria_personale_non_sanitario",
+																"categoria_ospiti_rsa",
+																"categoria_over80",
+															    "categoria_forze_armate",
+															    "categoria_personale_scolastico"],"mean3");
+	};
 	ixmaps.VACCINI_SOMMINISTRAZIONI_ALL_SEQUENCE_REVERS = function (theme, options) {
 		return ixmaps.VACCINI_COLUMNS_SEQUENCE(theme, options, ["categoria_over80",
 																"categoria_ospiti_rsa",
@@ -339,7 +349,15 @@ window.ixmaps = window.ixmaps || {};
 																"categoria_personale_non_sanitario",
 																"categoria_operatori_sanitari_sociosanitari"],"mean3");
 	};
-		
+	ixmaps.VACCINI_SOMMINISTRAZIONI_ALL_NEW_SEQUENCE_REVERS_MEAN_3 = function (theme, options) {
+		return ixmaps.VACCINI_COLUMNS_SEQUENCE(theme, options, ["categoria_personale_scolastico",
+																"categoria_forze_armate",
+																"categoria_over80",
+																"categoria_ospiti_rsa",
+																"categoria_personale_non_sanitario",
+																"categoria_operatori_sanitari_sociosanitari"],"mean3");
+	};
+
 	ixmaps.VACCINI_SOMMINISTRAZIONI_PRIMA_SECONDA_SEQUENCE = function (theme, options) {
 		return ixmaps.VACCINI_COLUMNS_SEQUENCE(theme, options, ["prima_dose","seconda_dose"], "");
 	};
@@ -347,6 +365,117 @@ window.ixmaps = window.ixmaps || {};
 	ixmaps.VACCINI_SOMMINISTRAZIONI_PRIMA_SECONDA_SEQUENCE_MEAN_3 = function (theme, options) {
 		return ixmaps.VACCINI_COLUMNS_SEQUENCE(theme, options, ["prima_dose","seconda_dose"], "mean3");
 	};
+		
+	ixmaps.VACCINI_SOMMINISTRAZIONI_PRIMA_SECONDA_SEQUENCE_REVERS = function (theme, options) {
+		return ixmaps.VACCINI_COLUMNS_SEQUENCE(theme, options, ["seconda_dose","prima_dose"], "");
+	};
+		
+	ixmaps.VACCINI_SOMMINISTRAZIONI_PRIMA_SECONDA_SEQUENCE_REVERS_MEAN_3 = function (theme, options) {
+		return ixmaps.VACCINI_COLUMNS_SEQUENCE(theme, options, ["seconda_dose","prima_dose"], "mean3");
+	};
+
+	
+    ixmaps.VACCINI_PERCENTUALI_POPOLAZIONE_CLIP = function (theme,options,valueColumnName) {
+		
+		var szUrl1 = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.csv";
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/ISTAT/DCIS_POPRES1_13032020145850184.csv";
+
+		// -----------------------------------------------------------------------------------------------               
+		// read the ArcGis Feature service
+		// ----------------------------------------------------------------------------------------------- 
+
+		var broker = new Data.Broker()
+		
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				
+			function (dataA) {
+			valueColumnName = valueColumnName || "prima_dose";
+			var mydata = dataA[0];
+			mydata.sort('data_somministrazione');
+
+			var dateA = mydata.column("data_somministrazione").values();
+			var date = dateA.pop();
+			date = new Date(date).toLocaleDateString();
+			ixmaps.setTitle("<f2 style='color:#888888;background-color:rgba(255,255,255,0.1);padding:0.3em 0.5em;border:#888888 solid 0.5px;border-radius:0.2em'>aggiornato: "+date+"</f2>");
+
+			var pivot = mydata.pivot(
+				{lead:"codice_NUTS2",
+				 keep:"nome_area",
+				 columns:"data_somministrazione",
+				 value:valueColumnName}
+			);
+
+			// get population lookup for incidence
+			var dataPop = dataA[1];
+			// correct region names in population table
+			dataPop.column("Territorio").map(function (value) {
+				if (value == "Emilia-Romagna") {
+					return "Emilia Romagna";
+				} 
+				return value;
+			});
+			var pop = [];
+			var terrA = dataPop.column("Territorio").values();
+			var popA = dataPop.column("Value").values();
+			for (var i = 0; i < terrA.length; i++) {
+				pop[terrA[i]] = popA[i];
+			}
+
+			pivot.column("Total").remove();
+			var indexName = pivot.column("nome_area").index;
+
+			var records = pivot.records;
+			var sumA = [];
+			for ( var r=0; r<records.length; r++ ){
+				sumA[r] = 0;
+			}
+			for ( var r=0; r<records.length; r++ ){
+				for ( var c=2; c<records[r].length; c++ ){
+					sumA[r] += Number(records[r][c]);
+					records[r][c] = (sumA[r] / pop[records[r][indexName].replace(/\-/," ")]*100).toFixed(6);
+				}
+			}
+
+			// get the columns with date 
+			var columns = pivot.columnNames();
+			columns.shift();
+			columns.shift();
+
+			var last = columns.length-1;
+
+			// and configure the theme
+			theme.szFields = columns.slice().join('|');
+			theme.szFieldsA = columns.slice();
+
+			// and set the label (for difference 1 less)
+			theme.szLabelA = columns.slice();
+
+			for ( var i=0; i<columns.length; i++ ){
+				columns[i] = new Date(columns[i]).toLocaleDateString();
+			}
+			theme.szLabelA = columns.slice();
+
+			theme.szXaxisA = columns.slice();
+
+			theme.nClipFrames = columns.length;
+
+			theme.szSnippet = "dal "+columns[0]+" al "+columns[last];
+
+			// -----------------------------------------------------------------------------------------------               
+			// deploy the data
+			// ----------------------------------------------------------------------------------------------- 
+
+			ixmaps.setExternalData(pivot, {
+				type: "dbtable",
+				name: options.name
+			});
+		});
+	};
+	
+	
+	
 		
 })();
 
