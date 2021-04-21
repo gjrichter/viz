9d59720a8bd89fcbe1b3ba761b21ba9b91c9deca
gjrichter <guenter.richter@gmail.com>
2021-04-21 23:21:04 +0200

    Update databroker_PCM_DPC_COVID_Regioni.js

--- COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
+++ COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
@@ -4655,7 +4655,7 @@ window.ixmaps = window.ixmaps || {};
 				theme.szXaxisA = szXaxisA;
 			
 				theme.szSnippet = "dal " + columns[0] + " al " + columns[last - 1];
-				ixmaps.setTitle("<f2 style='color:#888888;background-color:rgba(255,255,255,0.1);padding:0.3em 0.5em;border:#888888 solid 0.5px;border-radius:0.2em'>aggiornato: "+(columns[last - 1])+"</f2>");
+				ixmaps.setTitle("<span style='color:#666666;font-family:courier new,Raleway,arial,helvetica;'> date:"+columns[last - 1]+"</span>");
 
 		
 				// ----------------------------------------------------------------------------------------------- 
@@ -7068,7 +7068,7 @@ window.ixmaps = window.ixmaps || {};
 				theme.szXaxisA[theme.szXaxisA.length-1] = theme.szXaxisA[theme.szXaxisA.length-1];
 
 				theme.szSnippet = "dal " + columns[0] + " al " + columns[last - 1];
-				ixmaps.setTitle("<f2 style='color:#888888;background-color:rgba(255,255,255,0.1);padding:0.3em 0.5em;border:#888888 solid 0.5px;border-radius:0.2em'>aggiornato: "+(columns[last - 1])+"</f2>");
+				ixmaps.setTitle("<span style='color:#666666;font-family:courier new,Raleway,arial,helvetica;'> date:"+columns[last - 1]+"</span>");
 
 
 			
@@ -7193,7 +7193,7 @@ window.ixmaps = window.ixmaps || {};
 				theme.szXaxisA = szXaxisA;
 			
 				theme.szSnippet = "dal " + columns[0] + " al " + columns[last - 1];
-				ixmaps.setTitle("<f2 style='color:#888888;background-color:rgba(255,255,255,0.1);padding:0.3em 0.5em;border:#888888 solid 0.5px;border-radius:0.2em'>aggiornato: "+(columns[last - 1])+"</f2>");
+				ixmaps.setTitle("<span style='color:#666666;font-family:courier new,Raleway,arial,helvetica;'> date:"+columns[last - 1]+"</span>");
 
 
 			
