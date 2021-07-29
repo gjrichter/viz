40b8d283b259b68db952a3a8b55e1f1888fdb197
gjrichter <guenter.richter@gmail.com>
2021-07-29 18:15:10 +0200

    Update databroker_PCM_DPC_COVID_Regioni.js

--- COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
+++ COVID-19/projects/COVID-19-PCM_DPC/databroker_PCM_DPC_COVID_Regioni.js
@@ -4209,7 +4209,7 @@ window.ixmaps = window.ixmaps || {};
 				theme.szFieldsA = columns.slice();
 
 				// and set the label (for difference 1 less)
-				columns.shift();
+				//columns.shift();
 				theme.szLabelA = columns.slice();
 
 				theme.szXaxisA = __get_xaxis(columns);
