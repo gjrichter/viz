ffb6c7756e1dbe17d2fe79a1e6bbd1c637d00f49
gjrichter <guenter.richter@gmail.com>
2022-01-19 13:51:57 +0100

    Create dataprovider_ODS_COVID_open_record_sicilia.js

---
+++ COVID-19/projects/COVID-19-ODS/dataprovider_ODS_COVID_open_record_sicilia.js
@@ -0,0 +1,49 @@
+/**
+ * data provider for COVID-19 Maps
+ * loads data from ODS GitHub covid-open-report-sicilia
+ * parses into iXMaps data table format and param themes
+ */
+
+window.ixmaps = window.ixmaps || {};
+(function () {
+	
+	ixmaps.COVID_19_SICILIA_CASI_DIFF_LATEST.process = function(data,options){
+
+		var pivot = data.pivot({
+			lead: "pro_com_t",
+			columns: "data",
+			keep: "comune",
+			value: "casi"
+		  });
+
+		pivot.column("Total").remove();
+
+		/** get columns */
+		var columnsA = pivot.columnNames().slice(2);
+
+		/** set as data fields in actual theme */
+		options.theme.setProperties({
+			fields:columnsA.slice(-2).join("|")
+		});
+
+		/** set label and xaxis in actual theme */
+		options.theme.style.setProperties({
+			"snippet" :"al " + columnsA[columnsA.length-1]
+		});
+
+		/** set the date frame as map title */
+		ixmaps.setTitle("<span style='font-family:courier new,Raleway,arial,helvetica;font-size:24px;color:#444444'>" + " al " + columnsA[columnsA.length-1] +"</span>");
+
+		return pivot;
+	};
+
+ 	
+})();
+
+/**
+ * end of namespace
+ */
+
+// -----------------------------
+// EOF
+// -----------------------------
