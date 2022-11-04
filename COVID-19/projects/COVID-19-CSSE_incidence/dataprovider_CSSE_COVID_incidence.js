a842cc078ccdcdba1a6f992893d1359997164abf
gjrichter <guenter.richter@gmail.com>
2022-11-05 00:08:16 +0100

    Update dataprovider_CSSE_COVID_incidence.js

--- COVID-19/projects/COVID-19-CSSE_incidence/dataprovider_CSSE_COVID_incidence.js
+++ COVID-19/projects/COVID-19-CSSE_incidence/dataprovider_CSSE_COVID_incidence.js
@@ -83,6 +83,123 @@ window.ixmaps = window.ixmaps || {};
 	
 	
 	
+	ixmaps.CSSE_COVID_LAST_28_DAILY_INCIDENCE = function (theme, options) {
+		
+		// to get the last date in the time series 
+		var szUrl1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+		
+		// get country populations 
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/World/API_SP.POP.TOTL_DS2_en_csv_v2_1926760.csv";
+
+		new Data.Broker()
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				function (dataA) {
+					
+					var popA = dataA[1].lookupArray("2019","Country Name");
+					
+					popA["US"] = popA["United States"];
+					popA["Russia"] = popA["Russian Federation"];
+					popA["Czechia"] = popA["Czech Republic"];
+
+					for (var i in popA){
+						if (popA[i] < 1000000){
+						  popA[i] = null;
+						}
+					}
+
+					var confirmed_7 = __cumul_7(dataA[0]);
+					columnsA = confirmed_7.columnNames();
+					
+					for (var i = 4; i<columnsA.length; i++){
+						confirmed_7.column(columnsA[i]).map(function(value,row){
+							return (Number(value)/Number(popA[row[1]])*100000).toFixed(2);
+						})
+					}
+					
+					console.log(confirmed_7);
+					var label = columnsA.slice(-27);
+					var xaxis = columnsA.slice(-27);
+					
+					theme.szFields = columnsA.slice(-28).join("|");
+					
+					var date = columnsA.pop();
+					dateA = date.split("/");
+					date = new Date(Number("20" + dateA[2]), Number(dateA[0]) - 1, Number(dateA[1]), 18);
+					var lastDate = date.toLocaleDateString();
+					ixmaps.setTitle("<span style='font-family:courier new,Raleway,arial,helvetica;font-size:18px;color:#444444'>aggiornato al " + lastDate + "</span>", "right");
+					
+					// --------------------------------------
+					// deploy the data
+					// --------------------------------------
+					console.log(theme);
+					options.setData(confirmed_7, {
+						type: "dbtable",
+						name: options.name
+					});
+			});
+	};
+	
+
+	ixmaps.CSSE_COVID_LAST_28_DAILY_INCIDENCE_FATALITIES = function (theme, options) {
+		
+		// to get the last date in the time series 
+		var szUrl1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+		
+		// get country populations 
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/World/API_SP.POP.TOTL_DS2_en_csv_v2_1926760.csv";
+
+		new Data.Broker()
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				function (dataA) {
+					
+					var popA = dataA[1].lookupArray("2019","Country Name");
+					
+					popA["US"] = popA["United States"];
+					popA["Russia"] = popA["Russian Federation"];
+					popA["Czechia"] = popA["Czech Republic"];
+
+					for (var i in popA){
+						if (popA[i] < 1000000){
+						  popA[i] = null;
+						}
+					}
+
+					var confirmed_7 = __cumul_7(dataA[0]);
+					columnsA = confirmed_7.columnNames();
+					
+					for (var i = 4; i<columnsA.length; i++){
+						confirmed_7.column(columnsA[i]).map(function(value,row){
+							return (Number(value)/Number(popA[row[1]])*100000).toFixed(2);
+						})
+					}
+					
+					console.log(confirmed_7);
+					var label = columnsA.slice(-27);
+					var xaxis = columnsA.slice(-27);
+					
+					theme.szFields = columnsA.slice(-28).join("|");
+					
+					var date = columnsA.pop();
+					dateA = date.split("/");
+					date = new Date(Number("20" + dateA[2]), Number(dateA[0]) - 1, Number(dateA[1]), 18);
+					var lastDate = date.toLocaleDateString();
+					ixmaps.setTitle("<span style='font-family:courier new,Raleway,arial,helvetica;font-size:18px;color:#444444'>aggiornato al " + lastDate + "</span>", "right");
+					
+					// --------------------------------------
+					// deploy the data
+					// --------------------------------------
+					console.log(theme);
+					options.setData(confirmed_7, {
+						type: "dbtable",
+						name: options.name
+					});
+			});
+	};
+	
 	ixmaps.CSSE_COVID_LAST_56_DAILY_INCIDENCE = function (theme, options) {
 		
 		// to get the last date in the time series 
@@ -200,6 +317,123 @@ window.ixmaps = window.ixmaps || {};
 			});
 	};
 	
+	ixmaps.CSSE_COVID_ALL_WEEKLY_INCIDENCE = function (theme, options) {
+		
+		// to get the last date in the time series 
+		var szUrl1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+		
+		// get country populations 
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/World/API_SP.POP.TOTL_DS2_en_csv_v2_1926760.csv";
+
+		new Data.Broker()
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				function (dataA) {
+					
+					var popA = dataA[1].lookupArray("2019","Country Name");
+					
+					popA["US"] = popA["United States"];
+					popA["Russia"] = popA["Russian Federation"];
+					popA["Czechia"] = popA["Czech Republic"];
+
+					for (var i in popA){
+						if (popA[i] < 1000000){
+						  popA[i] = null;
+						}
+					}
+
+					var confirmed_7 = __every_7((dataA[0]));
+					columnsA = confirmed_7.columnNames();
+					
+					for (var i = 4; i<columnsA.length; i++){
+						confirmed_7.column(columnsA[i]).map(function(value,row){
+							return (Number(value)/Number(popA[row[1]])*100000).toFixed(2);
+						})
+					}
+					
+					var dataCols = columnsA.length-4;
+					var label = columnsA.slice(-(dataCols-1));
+					var xaxis = columnsA.slice(-(dataCols-1));
+					
+					theme.szFields = columnsA.slice(-dataCols).join("|");
+					
+					var date = columnsA.pop();
+					dateA = date.split("/");
+					date = new Date(Number("20" + dateA[2]), Number(dateA[0]) - 1, Number(dateA[1]), 18);
+					var lastDate = date.toLocaleDateString();
+					ixmaps.setTitle("<span style='font-family:courier new,Raleway,arial,helvetica;font-size:18px;color:#444444'>aggiornato al " + lastDate + "</span>", "right");
+					
+					// --------------------------------------
+					// deploy the data
+					// --------------------------------------
+					console.log(theme);
+					options.setData(confirmed_7, {
+						type: "dbtable",
+						name: options.name
+					});
+			});
+	};
+	
+	ixmaps.CSSE_COVID_ALL_WEEKLY_INCIDENCE_FATALITIES = function (theme, options) {
+		
+		// to get the last date in the time series 
+		var szUrl1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+		
+		// get country populations 
+		var szUrl2 = "https://s3.eu-west-1.amazonaws.com/data.ixmaps.com/World/API_SP.POP.TOTL_DS2_en_csv_v2_1926760.csv";
+
+		new Data.Broker()
+			.addSource(szUrl1, "csv")
+			.addSource(szUrl2, "csv")
+			.realize(
+				function (dataA) {
+					
+					var popA = dataA[1].lookupArray("2019","Country Name");
+					
+					popA["US"] = popA["United States"];
+					popA["Russia"] = popA["Russian Federation"];
+					popA["Czechia"] = popA["Czech Republic"];
+
+					for (var i in popA){
+						if (popA[i] < 1000000){
+						  popA[i] = null;
+						}
+					}
+
+					var confirmed_7 = __every_7(__mean_7(dataA[0]));
+					columnsA = confirmed_7.columnNames();
+					
+					for (var i = 4; i<columnsA.length; i++){
+						confirmed_7.column(columnsA[i]).map(function(value,row){
+							return (Number(value)/Number(popA[row[1]])*100000).toFixed(2);
+						})
+					}
+					
+					var dataCols = columnsA.length-4;
+					var label = columnsA.slice(-(dataCols-1));
+					var xaxis = columnsA.slice(-(dataCols-1));
+					
+					theme.szFields = columnsA.slice(-dataCols).join("|");
+					
+					var date = columnsA.pop();
+					dateA = date.split("/");
+					date = new Date(Number("20" + dateA[2]), Number(dateA[0]) - 1, Number(dateA[1]), 18);
+					var lastDate = date.toLocaleDateString();
+					ixmaps.setTitle("<span style='font-family:courier new,Raleway,arial,helvetica;font-size:18px;color:#444444'>aggiornato al " + lastDate + "</span>", "right");
+					
+					// --------------------------------------
+					// deploy the data
+					// --------------------------------------
+					console.log(theme);
+					options.setData(confirmed_7, {
+						type: "dbtable",
+						name: options.name
+					});
+			});
+	};
+	
+	
 	ixmaps.CSSE_COVID_LAST_54_WEEKLY_INCIDENCE = function (theme, options) {
 		
 		// to get the last date in the time series 
@@ -226,7 +460,7 @@ window.ixmaps = window.ixmaps || {};
 						}
 					}
 
-					var confirmed_7 = __every_7(__mean_7(dataA[0]));
+					var confirmed_7 = __every_7((dataA[0]));
 					columnsA = confirmed_7.columnNames();
 					
 					for (var i = 4; i<columnsA.length; i++){
